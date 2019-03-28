f3b8fa6173f9aa908b44387ee4e3c5a76007ccb7
Rory Woods <rory@preteckt.com>
2019-03-27 20:18:27 -0400

    Use quicker way to get vocab from mapping file.

--- beer.ai/vocabulary.py
+++ beer.ai/vocabulary.py
@@ -17,19 +17,15 @@ def create_vocab(category, out_file=None):
     if out_file == None:
         out_file = category + "_vocab.pickle"
 
-    # Load in correct column from HDF
-    col = cat + "_name"
-    with pd.HDFStore("all_recipes.h5", "r") as store:
-        df = store.select("ingredients", columns=[col])[col].dropna().drop_duplicates()
-    
     map_name = MAP_NAME.format(category)
     with open(map_name, "rb") as f:
         ing_map = pickle.load(f)
+    
+    ings = sorted(set(ing_map.values()))
+    vocab = {ing:i for i,ing in enumerate(ings)}
+    with open(out_file, "wb") as f:
+        pickle.dump(vocab, f)
 
-    # Map ingredients to cleaned names
-    df = df.apply(lambda x: ing_map.get(x))
-    # re-drop duplicates
-    df = df.drop_duplicates()
 
 def _setup_argparser():
     parser = argparse.ArgumentParser(
@@ -52,5 +48,6 @@ def _setup_argparser():
 
 
 if __name__=="__main__":
-    args = parse_args()
-    create_vocab(args.category, args.outfile)
+    parser = _setup_argparser()
+    args = parser.parse_args()
+    create_vocab(args.category, args.output_file)
