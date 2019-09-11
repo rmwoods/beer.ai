ffaa8b909e3ef73169aa8e6d40452ca8ee91a51e
Rory Woods <rory@preteckt.com>
2019-09-10 20:19:29 -0400

    Create master vocab instead of 1 per category.

--- beer.ai/vocabulary.py
+++ beer.ai/vocabulary.py
@@ -2,11 +2,10 @@ import argparse
 import pandas
 import pickle
 
-VALID_CATS = ["ferm", "yeast", "hop", "misc"]
+CATEGORIES = ["ferm", "yeast", "hop", "misc"]
 MAP_NAME = "{}map.pickle"
 
-
-def create_vocab(category, out_file=None):
+def create_vocab(out_file=None):
     """Given a category, load in the corresponding columns from the HDF and
     create unique id's for each ingredient. Valid categories:
         [ferm, yeast, hop, misc]
@@ -15,39 +14,33 @@ def create_vocab(category, out_file=None):
     the values from the _map.pickle files.
     """
     if out_file == None:
-        out_file = category + "_vocab.pickle"
+        out_file = "vocab.pickle"
 
-    map_name = MAP_NAME.format(category)
-    with open(map_name, "rb") as f:
-        ing_map = pickle.load(f)
-    
-    ings = sorted(set(ing_map.values()))
-    vocab = {ing:i for i,ing in enumerate(ings)}
+    vocab = {}
+    for category in CATEGORIES:
+        map_name = MAP_NAME.format(category)
+        with open(map_name, "rb") as f:
+            ing_map = pickle.load(f)
+            ings = sorted(set(ing_map.values()))
+            vocab.update({category + "_" + ing: i for i, ing in enumerate(ings, len(vocab))})
+            if category == "hop":
+                vocab.update({category + "_" + ing + "_dry": i for i, ing in enumerate(ings, len(vocab))})
+
+
     with open(out_file, "wb") as f:
         pickle.dump(vocab, f)
 
 
 def _setup_argparser():
     parser = argparse.ArgumentParser(
-        description="Program to create a \"vocabulatory\" from a list of "
-            "ingredients (unique index per ingredient)."
+        description='Program to create a "vocabulatory" from a list of '
+        "ingredients (unique index per ingredient)."
     )
-    parser.add_argument(
-        "-c",
-        "--category",
-        required=True,
-        choices=VALID_CATS,
-        help="Category to create vocab for."
-    )
-    parser.add_argument(
-        "-o",
-        "--output_file",
-        help="File to dump vocabulary to."
-    )
+    parser.add_argument("-o", "--output_file", help="File to dump vocabulary to.")
     return parser
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = _setup_argparser()
     args = parser.parse_args()
-    create_vocab(args.category, args.output_file)
+    create_vocab(args.output_file)
