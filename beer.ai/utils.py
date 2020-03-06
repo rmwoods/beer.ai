7ed75be5d362985a97314196599efc4f5221b2ef
Rory Woods <rory@preteckt.com>
2020-03-06 00:30:07 +0000

    - Stubbed out methods for ibu, color, and ABV - Added docstring for IBU

--- beer.ai/utils.py
+++ beer.ai/utils.py
@@ -1,4 +1,37 @@
 import json
 
-with open("styleguide.json") as f:
-    style_guide = json.load(f)
+
+def get_style_guide():
+    with open("styleguide.json") as f:
+        style_guide = json.load(f)
+
+
+def ibu(df):
+    """Return an estimated IBU given a beer recipe.
+
+    Parameters:
+    ===========
+        hops: dataframe
+            Dataframe containing scaled hop quantities and addition times.
+            Assumed columns are "hop_scaled", "hop_time"
+    Return:
+    ======
+        IBU: float
+        Estimate of the IBU for this set of hops (presumably corresponding to a
+        recipe.
+    """
+    pass
+
+
+def srm():
+    """
+    """
+    pass
+
+
+def color(*args, **kwargs):
+    return srm(*args, **kwargs)
+
+
+def abv():
+    pass
